c95115475122678af178ce3df206306948bc1746
StiflerPasha <dvoynospavel@gmail.com>
2019-12-31 13:52:25 +0300

    add pagination

--- src/apps/settings/components/SettingsBookmarks/SettingsBookmarksTable.vue
+++ src/apps/settings/components/SettingsBookmarks/SettingsBookmarksTable.vue
@@ -3,17 +3,49 @@
     <b-button
       v-b-modal.addUser
       class="pull-right"
-      size="lg"
+      size="md"
       variant="success"
     >
       {{ $t(`users.add`) }}
     </b-button>
+
+    <b-row align-h="center" class="my-2">
+      <b-col cols="5" class="per-page">
+        <b-form-group
+          label="Per page:"
+          label-size="sm"
+          label-cols="5"
+          label-align="right"
+          label-for="perPageSelect"
+          class="mb-0"
+        >
+          <b-form-select
+            v-model="perPage"
+            id="perPageSelect"
+            size="sm"
+            :options="pageOptions"
+          />
+        </b-form-group>
+      </b-col>
+      <b-col cols="5">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="totalRows"
+          :per-page="perPage"
+          align="fill"
+          size="sm"
+          class="my-0"
+        />
+      </b-col>
+    </b-row>
 
     <b-table
       striped
       hover
       :items="bookmarks"
       :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
       :sort-by.sync="sortBy"
       :sort-desc.sync="sortDesc"
     >
@@ -55,7 +87,7 @@
   import SettingsBookmarkForm from './SettingsBookmarkForm'
 
   export default {
-    props: ['bookmarks'],
+    props: ['bookmarks', 'length'],
     components: {SettingsBookmarkForm},
     data () {
       return {
@@ -83,8 +115,15 @@
             key: 'edit',
             label: ''
           }
-        ]
+        ],
+        totalRows: 1,
+        currentPage: 1,
+        perPage: 5,
+        pageOptions: [5, 20]
       }
+    },
+    mounted () {
+      this.totalRows = this.length
     },
     methods: {
       ...mapActions('backend', ['addBookmark', 'updateBookmark']),
@@ -103,5 +142,11 @@
 <style lang='scss' scoped>
   i {
     cursor:pointer;
+  }
+  .per-page {
+    max-width: 250px;
+    select {
+      max-width: 50px;
+    }
   }
 </style>
