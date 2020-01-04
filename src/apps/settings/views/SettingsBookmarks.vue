1ae8ef3d52666f0c7a3f41aaa5ef03bba69b14a2
StiflerPasha <dvoynospavel@gmail.com>
2020-01-04 18:00:12 +0300

    add error handler

--- src/apps/settings/views/SettingsBookmarks.vue
+++ src/apps/settings/views/SettingsBookmarks.vue
@@ -3,11 +3,12 @@
     <b-row>
       <b-col>
         <b-card :title="$t(`bookmarks_locale.bookmarks`)">
+          <Error v-if="isError"/>
           <SettingsBookmarksTable
+            v-else
             :bookmarks="bookmarks.data"
             :pending="pending"
-            :length="bookmarks.length"
-            @update="onUpdate"/>
+            :length="bookmarks.length"/>
         </b-card>
       </b-col>
     </b-row>
@@ -16,32 +17,33 @@
 
 <script>
   import SettingsBookmarksTable from '../components/SettingsBookmarks/SettingsBookmarksTable'
+  import Error from './404'
   import { mapActions, mapState } from 'vuex'
 
   export default {
     name: 'SettingsBookmarks',
     data () {
       return {
-        pending: true
+        pending: true,
+        isError: false
       }
     },
-    components: {
-      SettingsBookmarksTable
-    },
+    components: { SettingsBookmarksTable, Error },
     computed: {
       ...mapState('backend', ['bookmarks'])
     },
-    async created () {
-      await this.getBookmarks()
-      this.pending = false
-    },
-    methods: {
-      ...mapActions('backend', ['getBookmarks']),
-      async onUpdate () {
-        this.pending = true
+    async mounted () {
+      try {
         await this.getBookmarks()
         this.pending = false
+      } catch (err) {
+        this.isError = true
+        this.pending = false
+        this.$a.push({ type: 'danger', text: err })
       }
+    },
+    methods: {
+      ...mapActions('backend', ['getBookmarks'])
     }
   }
 </script>
